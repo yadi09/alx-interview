3f462bce80b10e18aefaf9b35e036a1799588363
Yadamzer <yadamzerterefe09@gmail.com>
2024-12-07 23:38:13 +0300

    Task 0

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""A Module"""
+
+
+def makeChange(coins, total):
+    """function
+    """
+    if total <= 0:
+        return 0
+
+    else:
+        coin = sorted(coins)
+        coin.reverse()
+        counter = 0
+        for i in coin:
+            while(total >= i):
+                counter += 1
+                total -= i
+        if total == 0:
+            return counter
+        return -1
